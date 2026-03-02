7797e19802f3448bcfe7ad26a0cd79dc09d1251d
Loredana Giaramita <giarafran@gmail.com>
2026-03-02 15:29:44 +0100

    Despliegues y HashRouter

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,17 +1,21 @@
 import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
+const loadFromLocalStorage = (): Character[] => {
+
+  const characters = localStorage.getItem('characters');
+
+  return characters ? JSON.parse(characters) : [];
+}
+
 @Injectable({ providedIn: 'root' })
 export class DragonballService {
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 9000 },
-  ]);
+  characters = signal<Character[]>(loadFromLocalStorage());
 
   saveToLocalStorage = effect(() => {
     /* console.log(`Character count ${this.characters().length}`);  */
-    localStorage.setItem('Characters', JSON.stringify(this.characters()))
+    localStorage.setItem('characters', JSON.stringify(this.characters()))
   })
 
   addCharacter(character: Character) {
