4d434ed053a9a497e6ba6377a46573ac68787d06
Loredana Giaramita <giarafran@gmail.com>
2026-03-11 10:20:24 +0100

    Gifservice- interfaces: klipyKey

--- 04-gifs-app/src/environments/environment.ts
+++ 04-gifs-app/src/environments/environment.ts
@@ -1,8 +1,11 @@
 export const environment = {
 
-production:true,
-companyName: 'Gifs',
-companyName2: 'App',
-companySlogan: 'Maneja tus gifs ',
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs ',
 
+  //ApiKeys
+  klipyKey: 'Q1rDTzK4SIGNRrLXW8xfRgZiBvjDqAy6Vveg1AFUumeYor4QcDukeaM74Fl6YZhR',
+  klipyURL: 'https://api.klipy.com/api/v1/Q1rDTzK4SIGNRrLXW8xfRgZiBvjDqAy6Vveg1AFUumeYor4QcDukeaM74Fl6YZhR/gifs/trending?per_page=25&customer_id=gifs&locale={locale}'
 };
