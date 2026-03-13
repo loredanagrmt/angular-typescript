2bb0a88d4916affeea717074411ff90fea3ff766
Loredana Giaramita <giarafran@gmail.com>
2026-03-13 13:30:45 +0100

    DaisyIU - Componentes

---
+++ 05-country-app/src/app/country/layouts/country-layout/country-layout.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-layout',
+  imports: [],
+  template: './country-layout.html',
+})
+export class CountryLayout {
+
+
+
+ }
