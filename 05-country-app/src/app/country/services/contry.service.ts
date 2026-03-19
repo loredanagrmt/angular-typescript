718d530dbefcd2148135d8d8699fe1ef369c6c63
Loredana Giaramita <giarafran@gmail.com>
2026-03-19 09:48:37 +0100

    feat: seccion 11 - debounce (busquedas automaticas)

--- 05-country-app/src/app/country/services/contry.service.ts
+++ 05-country-app/src/app/country/services/contry.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Inject, Injectable } from "@angular/core";
 import { RESTCountry } from "../interfaces/rest-contries.interface";
-import { catchError, delay, map, Observable, throwError } from "rxjs";
+import { catchError, delay, map, Observable, of, throwError } from "rxjs";
 import type { Country } from "../interfaces/country.interface";
 import { CountryMapper } from '../mapper/country.mapper';
 
